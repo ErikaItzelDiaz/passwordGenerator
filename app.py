3be9fa1d0c2aec0856938081ab42e0071d1cdc5e
Erika Diaz <erikaitzeldiaz@gmail.com>
2024-03-06 03:42:56 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import random
+
+from flask import Flask, render_template,request
+from dotenv import load_dotenv
+
+load_dotenv()
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+    @app.route("/password/",methods=["GET","POST"])
+    def password():
+        characters = list('abcdefghijklmnopqrstuvwxyz')
+        if request.form.get("uppercase"):
+            characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        if request.form.get("special"):
+            characters.extend(list('!@#$%&*'))
+        if request.form.get("numbers"):
+            characters.extend(list('1234567890'))
+        length = int(request.form.get("length"), 12)
+        password = ''
+        for x in range(length):
+            password += random.choice(characters)
+        return render_template("password.html",password=password)
+
+    @app.route("/about/")
+    def about():
+        return render_template("about.html")
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
